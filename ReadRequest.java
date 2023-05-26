4beca801884211a17111d3c4c1ca1f505fccd2cf
Khandaker Akif Razzak <omiakif@gmail.com>
2023-05-27 00:11:07 +0600

    capabilities of reading a file and outputing to the output stream has been added.

--- ReadRequest.java
+++ ReadRequest.java
@@ -1,5 +1,5 @@
 /**
- * The URL to trigger is: 
+ * The URL to trigger is: http://localhost:50505/RandomFolder/words.txt
  */
 
 
@@ -7,7 +7,8 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 import java.lang.Exception;
-import java.io.File;
+import java.io.*;
+
 /**
  * The main() program in this class is designed to read requests from
  * a Web browser and display the requests on standard output.  The
@@ -20,6 +21,7 @@ import java.io.File;
  * for new connections, until the program is terminated (by clicking the
  * red "stop" square in Eclipse or by Control-C on the command line).
  */
+
 public class ReadRequest {
 	
 	/**
@@ -28,8 +30,11 @@ public class ReadRequest {
 	 */
 	private final static int LISTENING_PORT = 50505;
 	
-	private final static String folderDirectory = "/mnt/test/RandomFolder/words.txt";
+	private final static String rootDirectory = "/mnt/test"; //This will vary from one server to another
+	
+	private static String pathFile = null;//This does not need to match for handshake
 
+	public static File file;
 	
 	
 	/**
@@ -41,6 +46,8 @@ public class ReadRequest {
 	 */
 	public static void main(String[] args) {
 		ServerSocket serverSocket;
+		
+		
 		try {
 			serverSocket = new ServerSocket(LISTENING_PORT);
 		}
@@ -48,20 +55,39 @@ public class ReadRequest {
 			System.out.println("Failed to create listening socket.");
 			return;
 		}
+		
+		
 		System.out.println("Listening on port " + LISTENING_PORT);
+		
+		
 		try {
 			while (true) {
+				
+				
 				Socket connection = serverSocket.accept();
-				System.out.println("\nConnection from " 
-						+ connection.getRemoteSocketAddress());
+				System.out.println("\nConnection from "  + connection.getRemoteSocketAddress());
 				handleConnection(connection);
+				
+				
+				
+				
 			}
+			
+			/**
+			 * This is beyond scope as the while loop will
+			 * keep on running infinitely without bound.
+			 * 
+			 * Nothing we write in this space will run
+			 */
+			
 		}
 		catch (Exception e) {
 			System.out.println("Server socket shut down unexpectedly!");
 			System.out.println("Error: " + e);
 			System.out.println("Exiting.");
 		}
+		
+		
 	}
 
 	/**
@@ -79,11 +105,16 @@ public class ReadRequest {
 	 *    communicate with the client.
 	 */
 	private static void handleConnection(Socket connection) {
-		int counter = 1;
-		String greenLight = null;
+		
+//		String status = " ";
 		
 		try {
 			Scanner in = new Scanner(connection.getInputStream());
+			
+			
+			
+			
+			
 			while (true) {
 				
 				if ( ! in.hasNextLine() )
@@ -98,25 +129,72 @@ public class ReadRequest {
 				
 
 					
-				if(tokens[0].contains("GET") && tokens[1].contains(folderDirectory) && (tokens[2].contains("HTTP/1.1") || tokens[2].contains("HTTP/1.1")) && counter == 1) {
+				if(tokens[0].contains("GET") && (tokens[2].contains("HTTP/1.1") || tokens[2].contains("HTTP/1.1"))) {
 					
-					System.out.println();
+
 					System.out.println();
 					
 					System.out.println("Handshake Confirmed!");
 					System.out.println();
-					System.out.println();
+					
+					pathFile = tokens[1];
+
+					//Test if the path provided is indeed a 
+					
+					file = new File(rootDirectory + tokens[1]); // Can read file
+					
+					if (file.isDirectory()) {
+						
+						System.out.println("Cannot send a directory!. Please write full path file");
+						System.out.println("--------------------");
+						System.out.println("--------------------");
+						
+						
+						
+					}else if(file.exists() && file.canRead()) {
+						
+						System.out.println("File exists and can be read");
+						System.out.println("--------------------");
+						System.out.println("--------------------");
+						
+						
+						PrintWriter outgoing = new PrintWriter(connection.getOutputStream()); //This can cause malicious way of intruding
+						
+						
+						outgoing.println("HTTP/1.1 200 OK \r\n");
+						
+						outgoing.println("Connection: close \r\n");
+						
+						outgoing.println("Content-Type: " + getMimeType(file.getName()) + " \r\n");
+						
+						outgoing.println("Content-Type: " + file.length() + " \r\n");
+						
+						outgoing.println();
+						
+						
+						outgoing.flush();
+						
+						
+						sendFile(file, connection.getOutputStream());
+						
+						
+					}
+
 					
 				}
 
 				System.out.println("  " + line);
 				
-				File file = new File(folderDirectory);
-						
+									
 			}
+			
+			
+			
 		}
 		catch (Exception e) {
+			
 			System.out.println("Error while communicating with client: " + e);
+			
 		}
 		finally {  // make SURE connection is closed before returning!
 			try {
@@ -127,5 +205,61 @@ public class ReadRequest {
 			System.out.println("Connection closed.");
 		}
 	}
+	
+	
+	/**
+	 * This is a method that will return the type of file as a String
+	 * @param fileName
+	 * @return returns the file type as String
+	 */
+	
+	
+	private static String getMimeType(String fileName) {
+        int pos = fileName.lastIndexOf('.');
+        if (pos < 0)  // no file extension in name
+            return "x-application/x-unknown";
+        String ext = fileName.substring(pos+1).toLowerCase();
+        if (ext.equals("txt")) return "text/plain";
+        else if (ext.equals("html")) return "text/html";
+        else if (ext.equals("htm")) return "text/html";
+        else if (ext.equals("css")) return "text/css";
+        else if (ext.equals("js")) return "text/javascript";
+        else if (ext.equals("java")) return "text/x-java";
+        else if (ext.equals("jpeg")) return "image/jpeg";
+        else if (ext.equals("jpg")) return "image/jpeg";
+        else if (ext.equals("png")) return "image/png";
+        else if (ext.equals("gif")) return "image/gif";
+        else if (ext.equals("ico")) return "image/x-icon";
+        else if (ext.equals("class")) return "application/java-vm";
+        else if (ext.equals("jar")) return "application/java-archive";
+        else if (ext.equals("zip")) return "application/zip";
+        else if (ext.equals("xml")) return "application/xml";
+        else if (ext.equals("xhtml")) return"application/xhtml+xml";
+        else return "x-application/x-unknown";
+           // Note:  x-application/x-unknown  is something made up;
+           // it will probably make the browser offer to save the file.
+     }
+	
+	
+	
+	/*
+	 * This is a method that will print the contents of the file
+	 * @param file 
+	 * @param OutputStream object
+	 * 
+	 */
+	
+	private static void sendFile(File file, OutputStream socketOut) throws IOException {
+	    InputStream in = new BufferedInputStream(new FileInputStream(file));
+	    OutputStream out = new BufferedOutputStream(socketOut);
+	    while (true) {
+	      int x = in.read(); // read one byte from file
+	      if (x < 0)
+	         break; // end of file reached
+	      out.write(x);  // write the byte to the socket
+	   }
+	   out.flush();
+	}
+	
 
 }
